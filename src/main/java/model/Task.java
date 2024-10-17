f5e946716530103bd920bdd8f20888a9b42568c1
KevinPernu <pernu@kth.se>
2024-10-17 23:52:36 +0200

    Made all methods for task class

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -1,10 +1,12 @@
 package model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
-public class Task {
-    private int id;
-    private String description;
+public class Task implements Comparable<Task>, Serializable {
+    private final int id;
+    private final String description;
     private String takenBy;
     private TaskState state;
     private TaskPrio prio;
@@ -17,8 +19,71 @@ public class Task {
         this.takenBy = takenBy;
         this.state = TaskState.TO_DO;
         this.prio = prio;
+        this.created = LocalDate.now();
         this.lastUpdated = created;
     }
 
+    public void setTakenBy(String takenBy) {
+        if (this.takenBy != null && !this.takenBy.isEmpty()) {
+            throw new IllegalStateException("Task is already taken by someone.");
+        }
+        this.takenBy = takenBy;
+        this.lastUpdated = LocalDate.now();
+    }
+
+    public void setState(TaskState state) {
+        this.state = state;
+        this.lastUpdated = LocalDate.now();
+    }
+
+    public void setPrio(TaskPrio prio) {
+        this.prio = prio;
+        this.lastUpdated = LocalDate.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTakenBy() {
+        return takenBy;
+    }
+
+    public TaskState getState() {
+        return state;
+    }
+
+    public TaskPrio getPrio() {
+        return prio;
+    }
+
+    public LocalDate getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    @Override
+    public int compareTo(Task other) {
+        int prioComparison = this.prio.compareTo(other.prio);
+        if (prioComparison != 0) {
+            return prioComparison;
+        }
+        return this.description.compareTo(other.description);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass()!= o.getClass()) return false;
+        Task task = (Task) o;
+        return description.equals(task.description) && prio == task.prio;
+    }
 
 }
